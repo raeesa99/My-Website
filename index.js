f09243695b7436af3cb46dc3f77ad57baf785ad6
raeesa99 <raeesapatel99@hotmail.com>
2022-12-26 14:39:13 +0000

    minor changes, complete

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ const express = require('express');
 const path = require('path');
 const { engine } = require('express-handlebars');
 
-// create an express server
 const app = express();
 
-// get express to serve static content to the user (images, css, js, etc)
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-// register handlebars as the templating engine
 app.engine('.hbs', engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', './views');
